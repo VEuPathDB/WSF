a638606f778105ec1657042c0f0463b5a84a519f
Xin Gao <xingao@upenn.edu>
2014-04-03 14:37:29 +0000

    remove test code.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WSF/trunk@13651 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Service/src/java/org/gusdb/wsf/service/WsfService.java
+++ Service/src/java/org/gusdb/wsf/service/WsfService.java
@@ -196,25 +196,25 @@ public class WsfService {
   private Map<String, Object> loadContext(String[] keys) {
     Map<String, Object> context = new HashMap<String, Object>();
 
-//    ServletContext scontext = null;
-//    MessageContext msgContext = MessageContext.getCurrentContext();
-//    if (msgContext != null) {
-//      Servlet servlet = (Servlet) msgContext.getProperty(HTTPConstants.MC_HTTP_SERVLET);
-//      scontext = servlet.getServletConfig().getServletContext();
-//    }
-//    if (scontext == null)
-//      scontext = SERVLET_CONTEXT;
-//    if (scontext != null) {
-//      // get the configuration path:
-//      String configPath = scontext.getRealPath(scontext.getInitParameter(Plugin.CTX_CONFIG_PATH));
-//      context.put(Plugin.CTX_CONFIG_PATH, configPath);
-//      for (String key : keys) {
-//        String initValue = scontext.getInitParameter(key);
-//        context.put(key, initValue);
-//        Object value = scontext.getAttribute(key);
-//        context.put(key, value);
-//      }
-//    }
+    ServletContext scontext = null;
+    MessageContext msgContext = MessageContext.getCurrentContext();
+    if (msgContext != null) {
+      Servlet servlet = (Servlet) msgContext.getProperty(HTTPConstants.MC_HTTP_SERVLET);
+      scontext = servlet.getServletConfig().getServletContext();
+    }
+    if (scontext == null)
+      scontext = SERVLET_CONTEXT;
+    if (scontext != null) {
+      // get the configuration path:
+      String configPath = scontext.getRealPath(scontext.getInitParameter(Plugin.CTX_CONFIG_PATH));
+      context.put(Plugin.CTX_CONFIG_PATH, configPath);
+      for (String key : keys) {
+        String initValue = scontext.getInitParameter(key);
+        context.put(key, initValue);
+        Object value = scontext.getAttribute(key);
+        context.put(key, value);
+      }
+    }
 
     return context;
   }
