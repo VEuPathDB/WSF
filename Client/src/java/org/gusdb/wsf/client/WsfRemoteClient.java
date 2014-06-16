ad45420322b5be1ac6c668c3c8082e0965724d29
Xin Gao <xingao@upenn.edu>
2014-06-16 17:11:47 +0000

    fix build script; throw exceptions if the response is not OK
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/gus/WSF/branches/wsfrestful@14052 4235af6a-31f9-0310-9ad3-ecf6348f2534

--- Client/src/java/org/gusdb/wsf/client/WsfRemoteClient.java
+++ Client/src/java/org/gusdb/wsf/client/WsfRemoteClient.java
@@ -53,6 +53,10 @@ public class WsfRemoteClient implements WsfClient {
     // invoke service
     Response response = target.request(MediaType.APPLICATION_OCTET_STREAM_TYPE).post(
         Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+    int status = response.getStatus();
+    if (status != 200)
+      throw new WsfClientException("Request failed with status code: " + status);
+
     InputStream inStream = null;
     int signal = 0;
     try {
